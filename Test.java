628507871a51850239f6a355593c878dfab85278
NateR4 <nrobertson4799@student.pps.net>
2023-05-10 09:57:59 -0700

    Today I learned how to read from a file and did troubleshooting

--- Test.java
+++ Test.java
@@ -1,20 +1,29 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 public class Test {
     //to do, search append to existing file
 
 
-    public static void main(String[] args) {
-        try {
-            BufferedWriter writer2 = new BufferedWriter(new FileWriter("output2.txt"));
-            //writer2.write("writing to a file!!");
-            writer2.append("\n This should be a line down");
+    public static void main(String[] args)throws IOException {
+            //Appending to a file
+            FileWriter file = new FileWriter("MyFile.txt",true);
+            BufferedWriter writer2 = new BufferedWriter(file);
+           // writer2.write("writing to a file!!");
+           // writer2.newLine();
             writer2.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+            file.close();
+
+            //Reading file
+        File fileobj = new File("C:\\Users\\nrobertson4799\\IdeaProjects\\ap-final-project-2-NateR4\\MyFile.txt");
+        Scanner input = new Scanner(fileobj);
+            while (input.hasNext() ){
+                String a = input.nextLine();
+                System.out.println(a);
+            }
     }
 
 }
